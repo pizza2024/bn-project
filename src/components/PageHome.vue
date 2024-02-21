e874e72e909254a89f4601f7f338d7ef30ce385c
pizza <tangzeyong@xbank.plus>
2024-02-21 18:38:22 +0800

    crypto list

--- src/components/PageHome.vue
+++ src/components/PageHome.vue
@@ -1,16 +1,14 @@
 <template>
-  <div class="flex">
-    <div class="flex flex-col basis-1/2 items-center px-2 py-4" :key="item.symbol" v-for="item in list">
-      <div class="flex gap-2 items-baseline">
-        <span class="text-2xl font-bold">
-          {{ BN(item.price).decimalPlaces(18).toString() }}
-        </span>
+  <div ref="wrapper" class="pb-14 h-dvh">
+    <div>
+      <div class="flex items-baseline gap-1 p-2" :key="item.symbol" v-for="item in list">
         <span class="text-md">
           {{ item.symbol?.replace('USDT', '') }}
         </span>
-      </div>
-      <div>≈</div>
-      <div class="flex gap-2 items-baseline">
+        <span class="text-2xl font-bold">
+          {{ BN(item.price).decimalPlaces(18).toString() }}
+        </span>
+        <div>≈</div>
         <span class="text-xl font-medium">
           {{ BN(item.price).multipliedBy(settingStore.preferRate).decimalPlaces(2).toString() }}
         </span>
@@ -20,18 +18,25 @@
       </div>
     </div>
   </div>
+  
 </template>
 <script setup>
+import BS from 'better-scroll'
 import BN from 'bignumber.js'
-import { onMounted, ref } from 'vue';
+import { nextTick, onMounted, ref } from 'vue';
 import { apiGetAllSymbolPrice } from '@/api/index'
 import { useSettingStore } from '@/store/settingStore';
+const wrapper = ref()
 const settingStore = useSettingStore()
 const list = ref([])
 onMounted(() => {
-  apiGetAllSymbolPrice({ symbols: '["ETHUSDT","BTCUSDT"]' }).then(resp => {
+  apiGetAllSymbolPrice().then(resp => {
     list.value = resp.data
-
+    nextTick(() => {
+      new BS(wrapper.value, {
+        click: true
+      })
+    })
   })
 
 })
