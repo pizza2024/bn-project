93655f38f1a3f84b8172ae08f808e8c93d77d814
pizza <tangzeyong@xbank.plus>
2024-02-21 18:48:15 +0800

    可以转化为usdt的对

--- src/components/PageHome.vue
+++ src/components/PageHome.vue
@@ -1,16 +1,16 @@
 <template>
-  <div ref="wrapper" class="pb-14 h-dvh">
-    <div>
+  <div ref="wrapper" class="h-dvh">
+    <div class="pb-14">
       <div class="flex items-baseline gap-1 p-2" :key="item.symbol" v-for="item in list">
         <span class="text-md">
           {{ item.symbol?.replace('USDT', '') }}
         </span>
         <span class="text-2xl font-bold">
-          {{ BN(item.price).decimalPlaces(18).toString() }}
+          {{ BN(item.price).decimalPlaces(18).toFormat() }}
         </span>
         <div>≈</div>
         <span class="text-xl font-medium">
-          {{ BN(item.price).multipliedBy(settingStore.preferRate).decimalPlaces(2).toString() }}
+          {{ BN(item.price).multipliedBy(settingStore.preferRate).decimalPlaces(18).toFormat() }}
         </span>
         <span class="text-sm">
           {{ settingStore.preferCurrency?.replaceAll(/(_USD)|(USD_)/g, '') }}
@@ -31,10 +31,13 @@ const settingStore = useSettingStore()
 const list = ref([])
 onMounted(() => {
   apiGetAllSymbolPrice().then(resp => {
-    list.value = resp.data
+    list.value = resp.data?.filter(x => {
+      return x.symbol?.includes('USDT')
+    })
     nextTick(() => {
       new BS(wrapper.value, {
-        click: true
+        click: true,
+        useTransition: false
       })
     })
   })
