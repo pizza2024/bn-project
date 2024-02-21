ea141e0d9fdd590cd67acc6f9ca053a55abcb19d
ttzy2022 <tangzeyong@xbank.plus>
2024-02-21 23:06:25 +0800

    增加搜索框

--- src/components/PageHome.vue
+++ src/components/PageHome.vue
@@ -1,34 +1,68 @@
 <template>
   <div ref="wrapper" class="h-dvh">
-    <div class="pb-14">
-      <div class="flex items-baseline gap-1 p-2" :key="item.symbol" v-for="item in list">
-        <span class="text-md">
-          {{ item.symbol?.replace('USDT', '') }}
-        </span>
-        <span class="text-2xl font-bold">
-          {{ BN(item.price).decimalPlaces(18).toFormat() }}
-        </span>
-        <div>≈</div>
-        <span class="text-xl font-medium">
-          {{ BN(item.price).multipliedBy(settingStore.preferRate).decimalPlaces(18).toFormat() }}
-        </span>
-        <span class="text-sm">
-          {{ settingStore.preferCurrency?.replaceAll(/(_USD)|(USD_)/g, '') }}
-        </span>
+    <div class="py-14">
+      <div class="p-2" :key="item.symbol" v-for="item in displayList">
+        <div class="flex items-baseline gap-1">
+          <div class="invisible">≈</div>
+          <span class="text-xl font-bold">1</span>
+          <span class="text-sm">
+            {{ item.symbol?.replace('USDT', '') }}
+          </span>
+          <!-- <div>≈</div>
+          <span class="text-xl font-bold">
+            {{ BN(item.price).decimalPlaces(18).toFormat() }}
+          </span>
+          <span class="text-sm">USDT</span> -->
+        </div>
+        <div class="flex items-baseline gap-1">
+          <!-- <span class="text-md invisible">
+            1 {{ item.symbol?.replace('USDT', '') }}
+          </span> -->
+          <div>≈</div>
+          <span class="text-xl font-bold">
+            {{ BN(item.price).decimalPlaces(18).toFormat() }}
+          </span>
+          <span class="text-sm">USDT</span>
+        </div>
+        <div class="flex items-baseline gap-1">
+          <!-- <span class="text-md invisible">
+            1 {{ item.symbol?.replace('USDT', '') }}
+          </span> -->
+          <div>≈</div>
+          <span class="text-lg font-medium">
+            {{ BN(item.price).multipliedBy(settingStore.preferRate).decimalPlaces(18).toFormat() }}
+          </span>
+          <span class="text-sm">
+            {{ settingStore.preferCurrency?.replaceAll(/(_USD)|(USD_)/g, '') }}
+          </span>
+        </div>
       </div>
     </div>
   </div>
-  
+  <div class="h-14 fixed top-0 inset-x-0 bg-white flex items-center p-2">
+    <input v-model="state.query" type="text" class="border border-black p-2 grow rounded-lg">
+    <IconXCircle v-if="!!state.query" class="fixed right-4 inset-y-center" @click="state.query = ''"/>
+  </div>
 </template>
 <script setup>
+import _ from 'lodash'
 import BS from 'better-scroll'
 import BN from 'bignumber.js'
-import { nextTick, onMounted, ref } from 'vue';
+import { nextTick, onMounted, ref, reactive, computed } from 'vue';
 import { apiGetAllSymbolPrice } from '@/api/index'
 import { useSettingStore } from '@/store/settingStore';
+import IconXCircle from '@/components/IconXCircle.vue'
 const wrapper = ref()
 const settingStore = useSettingStore()
 const list = ref([])
+const state = reactive({
+  query: ''
+})
+const displayList = computed(() => {
+  return list.value.filter(x => {
+    return _.upperCase(x.symbol)?.includes(_.upperCase(state.query))
+  })
+})
 onMounted(() => {
   apiGetAllSymbolPrice().then(resp => {
     list.value = resp.data?.filter(x => {
